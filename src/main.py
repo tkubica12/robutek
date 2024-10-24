04d9d5cca510669118d5653b354ac01cf6c72c91
Tomas Kubica <tkubica12@gmail.com>
2024-10-24 13:20:06 +0200

    final line sensor refactor

--- src/main.py
+++ src/main.py
@@ -52,16 +52,6 @@ right_speed_sensor = Pin(35, Pin.IN, Pin.PULL_DOWN)
 left_speed_sensor.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=r.motors_controller.left_speed_sensor.process_event)
 right_speed_sensor.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=r.motors_controller.right_speed_sensor.process_event)
 
-# Register callbacks for line sensors
-# left_tracking_sensor = Pin(21, Pin.IN, Pin.PULL_DOWN)
-# center_left_tracking_sensor = Pin(22, Pin.IN, Pin.PULL_DOWN)
-# center_right_tracking_sensor = Pin(23, Pin.IN, Pin.PULL_DOWN)
-# right_tracking_sensor = Pin(5, Pin.IN, Pin.PULL_DOWN)
-# left_tracking_sensor.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=r.line_tracking.left_sensor_to_event)
-# center_left_tracking_sensor.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=r.line_tracking.center_left_sensor_to_event)
-# center_right_tracking_sensor.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=r.line_tracking.center_right_sensor_to_event)
-# right_tracking_sensor.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=r.line_tracking.right_sensor_to_event)
-
 # Features configuration
 r.enable_adaptive_cruise_control(False)
 r.enable_follow_the_line(False)
@@ -70,14 +60,4 @@ r.enable_speed_regulation(False)
 sleep_ms(5000)
 
 r.robot_state_machine.handle_event(StartMoving(CONFIG["DEMO_FORWARD_SPEED"], Direction.FORWARD))
-
-# r.movement_controller.motors_controller.stationary_turn(0.5, TurnDirection.CLOCKWISE)
-# r.movement_controller.drive_desired_state(0.25, Direction.FORWARD)
-# r.motors_controller.calibrate_advanced(r.motors_controller.left_motor, r.motors_controller.left_speed_sensor, "left_calibration.csv")
-# r.motors_controller.stop()
-# r.motors_controller.calibrate(r.motors_controller.left_motor, r.motors_controller.left_speed_sensor)
-# r.motors_controller.left_motor.forward(40000)
-# r.motors_controller.drive(0.2, Direction.FORWARD)
-# r.motors_controller.drive(0.2, Direction.BACKWARD)
-# r.movement_controller.drive_desired_state(0.15, Direction.BACKWARD)
 
